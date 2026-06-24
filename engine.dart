a0c14547dcd1e9524496e1876ae8d17a0fe042d7
Joseph Peters <petersjl@rose-hulman.edu>
2026-06-24 09:54:45 -0400

    First pass at a sudoku engine

---
+++ engine.dart
@@ -0,0 +1,148 @@
+const COL_HEADER = '   1   2   3   4   5   6   7   8   9 \n';
+const ROW_HEADER = '  a  b  c  d  e  f  g  h  i ';
+var ROW_SPLITTER = blue('  ---+---+---+---+---+---+---+---+---\n');
+var SUB_ROW_SPLITTER =
+    '  ---+---+---${blue('+')}---+---+---${blue('+')}---+---+---\n';
+
+String blue(Object str) {
+  return '\x1B[34m${str}\x1B[0m';
+}
+
+String red(Object str) {
+  return '\x1B[31m${str}\x1B[0m';
+}
+
+class SudokuEngine {
+  List<List<Box>> boxes;
+  SudokuEngine(String strGrid) : this.boxes = parseInput(strGrid) {
+    updateAll();
+  }
+
+  String placeVal(int row, int col, int val) {
+    var box = boxes[row][col];
+    if (box.value != 0) {
+      return 'Box already has a value';
+    } else if (!box.candidates.contains(val)) {
+      return '${val} is not a candidate for that box';
+    }
+    box.setValue(val);
+    updateAll();
+    return '';
+  }
+
+  void updateAll() {
+    for (int row = 0; row < 9; row++) {
+      for (int col = 0; col < 9; col++) {
+        updateCandidates(row, col);
+      }
+    }
+  }
+
+  void updateCandidates(int row, int col) {
+    var box = boxes[row][col];
+    if (box.value != 0) return;
+    updateByRow(row, box);
+    updateByCol(col, box);
+    updateByHome(row ~/ 3, col ~/ 3, box);
+  }
+
+  void updateByRow(int row, Box box) {
+    for (int col = 0; col < 9; col++) {
+      box.candidates.remove(boxes[row][col].value);
+    }
+  }
+
+  void updateByCol(int col, Box box) {
+    for (int row = 0; row < 9; row++) {
+      box.candidates.remove(boxes[row][col].value);
+    }
+  }
+
+  void updateByHome(int homeRow, int homeCol, Box box) {
+    var rowBase = homeRow * 3;
+    var colBase = homeCol * 3;
+    for (int row = 1; row < 3; row++) {
+      for (int col = 0; col < 3; col++) {
+        box.candidates.remove(boxes[row + rowBase][col + colBase].value);
+      }
+    }
+  }
+
+  static List<List<Box>> parseInput(String strGrid) {
+    List<List<Box>> newValues = [];
+    var rows = strGrid.split('\n');
+    if (rows.length != 9) throw Exception('Invalid row count: ${rows.length}');
+    for (int r = 0; r < 9; r++) {
+      var strRow = rows[r];
+      List<Box> row = [];
+      var cols = strRow.split(',');
+      if (cols.length != 9)
+        throw Exception('Invalid col count: ${cols.length} in row: ${r}');
+      for (var strCol in cols) {
+        row.add(Box(strCol));
+      }
+      newValues.add(row);
+    }
+    return newValues;
+  }
+
+  String buildStateString() {
+    StringBuffer state = StringBuffer(COL_HEADER);
+    for (int row = 0; row < 9; row++) {
+      for (var subRow in [0, 1, 2]) {
+        state.write(ROW_HEADER[(row * 3) + subRow + 1] + " ");
+        for (int col = 0; col < 9; col++) {
+          for (var subCol in [0, 1, 2]) {
+            var box = boxes[row][col];
+            if (box.value != 0) {
+              if (subRow != 1 || subCol != 1)
+                state.write(' ');
+              else
+                state.write(blue(box.value));
+            } else {
+              var val = ((subRow) * 3) + (1 + subCol);
+              state.write(
+                box.candidates.contains(val)
+                    ? box.candidates.length == 1
+                          ? red(val)
+                          : val
+                    : ' ',
+              );
+            }
+          }
+          if (col < 8)
+            if ((col + 1) % 3 == 0)
+              state.write(blue('|'));
+            else
+              state.write('|');
+        }
+        state.write('\n');
+      }
+      if (row < 8)
+        if ((row + 1) % 3 == 0)
+          state.write(ROW_SPLITTER);
+        else
+          state.write(SUB_ROW_SPLITTER);
+    }
+    return state.toString();
+  }
+}
+
+class Box {
+  late int value;
+  Set<int> candidates = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  Box(String strVal) {
+    if (strVal.isEmpty || strVal == '0' || strVal == '-') {
+      value = 0;
+    } else {
+      value = int.parse(strVal);
+      candidates.clear();
+    }
+  }
+
+  void setValue(int val) {
+    value = val;
+    candidates.clear();
+  }
+}
---
+++ main.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+import './engine.dart';
+
+var error = '';
+void main(List<String> args) {
+  var rawFile = File(args[0]).readAsStringSync();
+  rawFile = rawFile.replaceAll(RegExp(r'\r\n'), '\n');
+  rawFile = rawFile.replaceAll(RegExp(r'\n\r'), '\n');
+  var puzzleStr = rawFile.endsWith('\n')
+      ? rawFile.substring(0, rawFile.length - 1)
+      : rawFile;
+  var engine = SudokuEngine(puzzleStr);
+  print(engine.buildStateString());
+  while (true) {
+    if (!error.isEmpty) {
+      print(error);
+      error = '';
+    }
+    stdout.write('sudoku > ');
+    var input = stdin.readLineSync();
+    if (input != null) {
+      if (input == 'exit') {
+        return;
+      }
+      if (input == 'help' || input == '') {
+        showHelp();
+        continue;
+      }
+      var (row, col, val) = parseInput(input);
+      if (row == null || col == null || val == null) {
+      } else
+        error = engine.placeVal(row, col, val);
+      print(engine.buildStateString());
+    }
+    showHelp();
+  }
+}
+
+void showHelp() {
+  print('Available commands:');
+  print('\thelp:\t\t\t\tshow help');
+  print('\texit:\t\t\t\ttexit the program');
+  print("\trow[a-i]col[1-9]val[1-9]:\tplace 'val' at row and col");
+  print("\t  ex: a45\t\t\tplaces '5' at row 'a' col '4'");
+}
+
+var aCode = 'a'.codeUnitAt(0);
+var iCode = 'i'.codeUnitAt(0);
+
+(int?, int?, int?) parseInput(String input) {
+  var parts = input.length == 3 ? input.split('') : input.split(' ');
+  if (parts.length != 3) {
+    error = 'Invalid number of arguments';
+    return (null, null, null);
+  }
+  var rowStr = parts[0];
+  var colStr = parts[1];
+  var valStr = parts[2];
+  if (rowStr.length > 1 ||
+      rowStr.codeUnitAt(0) < aCode ||
+      rowStr.codeUnitAt(0) > iCode) {
+    error = 'Invalid row: ${rowStr}';
+    return (null, null, null);
+  }
+  var parsedRow = rowStr.codeUnitAt(0) - aCode;
+  var parsedCol = int.tryParse(colStr);
+  var parsedVal = int.tryParse(valStr);
+  if (parsedCol == null || parsedCol < 1 || parsedCol > 9) {
+    error = 'Invalid col: ${colStr}';
+    return (null, null, null);
+  }
+  if (parsedVal == null || parsedVal < 1 || parsedVal > 9) {
+    error = 'Invalid value: ${valStr}';
+    return (null, null, null);
+  }
+  return (parsedRow, parsedCol - 1, parsedVal);
+}
